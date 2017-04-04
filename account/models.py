dc9497b6ebe1ef8a6245553707e2e14daafba8ed
Reinier Hernandez Avila <rhernandeza@uho.edu.cu>
2017-04-03 23:17:17 -0400

    mostrando la imagen del perfil del usuario

--- account/models.py
+++ account/models.py
@@ -9,8 +9,13 @@ SEX_CHOICES = (
 )
 
 
+def url(self, filename):
+    route = 'users/{}/{}'.format(self.username, filename)
+    return route
+
+
 class Profile(User):
-    picture = models.ImageField('Imagen', blank=True, null=True)
+    picture = models.ImageField('Imagen', blank=True, null=True, upload_to=url)
     born_date = models.DateField('fecha de nacimiento', blank=True, null=True)
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, default='U')
 
--- airport/urls.py
+++ airport/urls.py
@@ -1,22 +1,10 @@
-"""airport URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from account.views import DashboardView
+from airport import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -25,3 +13,5 @@ urlpatterns = [
     url(r'^model/', include('model.urls', namespace='model')),
     url(r'^$', DashboardView.as_view(), name='dashboard'),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
