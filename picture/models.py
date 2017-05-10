1fa76f2455b7c9fb99043169e4a79b5d33fd6321
Reinier Hernández Ávila <rhernandeza@uho.edu.cu>
2017-05-09 22:06:47 -0400

    mostrando las fotos subidas en orden ascendente

--- picture/models.py
+++ picture/models.py
@@ -12,6 +12,7 @@ class Image(models.Model):
     image = models.ImageField('imagen', upload_to=url, )
     up_by = models.ForeignKey(Profile, verbose_name='Subido por')
     description = models.CharField('descripcion', max_length=100, blank=True)
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{} --> {}'.format(self.description, self.up_by)
--- picture/views.py
+++ picture/views.py
@@ -9,6 +9,7 @@ from picture.models import Image
 
 class PictureListView(ProfileMixin, ListView):
     model = Image
+    queryset = Image.objects.all().order_by('-date')
 
 
 class PictureCreateView(ProfileMixin, CreateView):
@@ -32,5 +33,5 @@ class PresentationView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(PresentationView, self).get_context_data(**kwargs)
-        context['picture_list'] = Image.objects.all()
+        context['picture_list'] = Image.objects.all().order_by('-date')
         return context
